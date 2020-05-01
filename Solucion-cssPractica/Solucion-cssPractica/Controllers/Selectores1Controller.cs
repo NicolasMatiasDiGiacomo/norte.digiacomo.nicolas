ae0b28269a397dec3e3e4fde98495ef638f34a2e
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-05-01 14:47:56 -0300

    selectores 1

---
+++ Solucion-cssPractica/Solucion-cssPractica/Controllers/Selectores1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Solucion_cssPractica.Controllers
+{
+    public class Selectores1Controller : Controller
+    {
+        // GET: Selectores1
+        public ActionResult Selectores1()
+        {
+            return View();
+        }
+    }
+}
