0b666c5b527e5b5454ab2935e0ed29f5ac5fda7c
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-05-01 13:39:52 -0300

    mvc + primer ejercicio de css

---
+++ Solucion-cssPractica/Solucion-cssPractica/Controllers/ComoSeUsa1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Solucion_cssPractica.Controllers
+{
+    public class ComoSeUsa1Controller : Controller
+    {
+        // GET: ComoSeUsa1
+        public ActionResult ComoSeUsa1()
+        {
+            return View();
+        }
+    }
+}
