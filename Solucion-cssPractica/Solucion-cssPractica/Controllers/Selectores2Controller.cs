c7bda7e7bf4d78cde4342dc774d97d4c1945f005
Nicolas Matias Di Giacomo <nicoelnarigon@hotmail.com>
2020-05-01 16:08:02 -0300

    Selector 4

---
+++ Solucion-cssPractica/Solucion-cssPractica/Controllers/Selectores2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Solucion_cssPractica.Controllers
+{
+    public class Selectores2Controller : Controller
+    {
+        // GET: Selectores2
+        public ActionResult Selectores2()
+        {
+            return View();
+        }
+    }
+}
